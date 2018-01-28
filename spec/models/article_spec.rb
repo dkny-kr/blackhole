0a35ca5f3d425eddd88931b7f4cbdb1e150fa1ea
DG <i@dizy.me>
2018-01-28 15:47:28 +0900

    Article 모델의 Validation 스펙 작성
    ISSUE: #4

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validation' do
+    it { should validate_presence_of(:user_id) }
+    it { should validate_presence_of(:url) }
+    it { should validate_presence_of(:title) }
+  end
 end
