68b0489fe5b9ae907aa6249e9aa17d2e17f1956a
DG <i@dizy.me>
2018-01-28 16:54:48 +0900

    Article 모델 validation spec 구현
    ISSUE: #4

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,9 @@
+require 'uri'
+
 class Article < ApplicationRecord
+  belongs_to :user
+
+  validates :user_id, presence: true
+  validates :url, presence: true, format: /#{URI::regexp}/
+  validates :title, presence: true
 end
