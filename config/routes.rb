04884857afba27230981822f102b8dddd32d0f53
DG <i@dizy.me>
2018-01-28 23:14:04 +0900

    devise controller 추가
    ISSUE: #4

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'articles/index'
+
+  get 'articles/new'
+
+  get 'articles/show'
+
+  root 'articles#index'
+
+  resource :articles
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
