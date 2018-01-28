2c6dc2ecaacf99ef9101c332adc8b22b5eb4a50f
DG <i@dizy.me>
2018-01-28 16:49:15 +0900

    FactoryBot 추가
    ISSUE: #1

---
+++ spec/factories.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  to_create do |instance|
+	unless instance.save
+	  puts instance.inspect
+	  raise "Invalid record: #{instance.errors.full_messages.join(', ')}"
+	end
+  end
+  factory :user do
+    username 'USER'
+    email 'test@user.com'
+    password '12341234'
+    password_confirmation '12341234'
+  end
+end
